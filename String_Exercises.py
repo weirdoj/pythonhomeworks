58af52ddf16f91a22be49789a86edf542801a84d
Jimmy <8157881+kiajimmy@users.noreply.github.com>
2021-05-31 14:56:53 +1200

    Create String_Exercises.py

---
+++ String_Exercises.py
@@ -0,0 +1,22 @@
+# exercise 1
+for c in "Camus":
+    print(c)
+    
+# exercise 2
+print("aldous was born in 1894.".capitalize())
+
+
+# exercise 3
+print("Where now? Who now? When now?".split('?'))
+
+# exercise 4
+print(' '.join(["The", "fox", "jumped", "over", "the", "fence", "."]))
+
+# exercise 5
+print("three" + "three" + "three")
+print("three" * 3)
+
+#exercise 6
+#use \ to display \" Text in here \" in quote
+print("She said \"Surely\"")
+
