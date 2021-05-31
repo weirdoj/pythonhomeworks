83156ef6b01cfab49c1dea12033e61928baac318
Jimmy <8157881+kiajimmy@users.noreply.github.com>
2021-05-31 14:59:35 +1200

    Update String_Exercises.py

--- String_Exercises.py
+++ String_Exercises.py
@@ -20,3 +20,21 @@ print("three" * 3)
 #use \ to display \" Text in here \" in quote
 print("She said \"Surely\"")
 
+#exercise 7
+my_string = "We are holding the true".capitalize()
+my_string1 = "We are holding the true".upper()
+print(my_string)
+print(my_string1)
+
+#exercise 8
+n1 = input("Enter a noun: ")
+
+v1 = input("Enter a verd: ")
+
+adj = input("Enter an adjective: ")
+
+n2 = input("Enter a noun: ")
+
+r = """ The {} {} the {} {} """.format(n1, v1, adj, n2)
+print(r)
+
